1f07398d9fb82aac1ecb77dd5a0f9be8251cdc9d
Lintang Kusuma Adjie <lintang641@gmail.com>
2020-09-28 11:52:14 +0700

    Update helpers.js

--- tugas1_ppl/src/helpers.js
+++ tugas1_ppl/src/helpers.js
@@ -8,3 +8,28 @@ function recalculateSubtotal(price, qty) {
 function total(price, qty, kode){
     return recalculateSubtotal(qty, price) - hitungDiskon(price, qty, kode);
 }
+
+function absoluteQty(qty) {
+    return (parseInt(qty) < 1) ? 1 : qty;
+}
+
+function hitungDiskon(price, qty, kode) {
+    const subTotal = recalculateSubtotal(price, qty);
+    switch (kode) {
+        case "KODE10":
+            hasilDiskon = subTotal * 0.10;
+            break;
+        case "KODE25":
+            hasilDiskon = subTotal * 0.25;
+            break;
+        case "KODE50":
+            hasilDiskon = subTotal * 0.50;
+            break;
+            hasilDiskon = 0;
+            break;
+    }
+    return hasilDiskon;
+}
+
+
+    
